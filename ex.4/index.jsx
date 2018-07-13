5f34f0a524ca8d0251410ba207bdd589a6474b0c
leonardomarciano <leonardo@marciano.io>
2018-07-13 14:52:28 -0300

    Ensinando React para babacas

---
+++ ex.4/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Primeiro, Segundo} from './Component'
+
+ReactDOM.render(
+    <div>
+    <Primeiro r1="Soooool" />
+    <Segundo r2="skirrr"/>
+    </div>
+    
+    , document.getElementById("app"))
---
+++ ex/manoFamily.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name="Raffa Moreira " lastName="Mano" />
+        <Member name="Leoffa Moreira " lastName="Mano" />
+        <Member name="Daniffa Moreira " lastName="Mano" />
+    </div>
+)
